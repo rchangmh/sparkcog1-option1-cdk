17d3585bbf70b2b8a5ddec372918ad3d78053c3b
Ronald Chang <rchangmh@gmail.com>
2022-02-09 18:28:59 -0500

    Initial commit

---
+++ test/sparkcog-option1-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SparkcogOption1Cdk from '../lib/sparkcog-option1-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sparkcog-option1-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SparkcogOption1Cdk.SparkcogOption1CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
